56e873e90ed8383f3f0e32aa4242f28da39f2132
grubsteak <joel.herzog@att.net>
2022-12-29 22:13:11 -0600

    rename sleep() to msleep()

--- src/timer/tinspire/SDL_systimer.c
+++ src/timer/tinspire/SDL_systimer.c
@@ -21,12 +21,13 @@
 */
 
 #include <os.h>
+
 #include "SDL_config.h"
 
 #ifdef SDL_TIMER_TINSPIRE
 
-#include "SDL_timer.h"
 #include "../SDL_timer_c.h"
+#include "SDL_timer.h"
 
 static volatile unsigned *value;
 static volatile unsigned *control;
@@ -39,38 +40,35 @@ TC: http://hackspire.unsads.com/wiki/index.php/Memory-mapped_I/O_ports#900C0000_
 CX: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0271d/Babehiha.html  
 */
 
-void SDL_StartTicks(void)
-{
-	*(volatile unsigned *)0x900B0018 &= ~(1 << 11); /* Enable bus access */
-	if ( __has_colors ) {
-		value = (unsigned *)0x900C0004;
-		control = (unsigned *)0x900C0008;
-		*(volatile unsigned *)0x900C0080 = 0xA; /* "[...] on the CX to specify the 32768Hz clock as the source for the First Timer" */
-		*control = 0b10000010; /* Wrapping; 32-bit; divider to 1; interrupt disabled; free-running; start */
-		start = *value;
-	} else {
-		value = (unsigned *)0x900C0000;
-		control = (unsigned *)0x900C0008;
-		*(volatile unsigned *)0x900C0004 = 33; /* Set time divider to 33; 32768/33=993Hz (approx. 1000Hz) */
-		*control = 0b00001111; /* Run infinitely; increasing; start */
-		*value = 0;
-	}
+void SDL_StartTicks(void) {
+    *(volatile unsigned *)0x900B0018 &= ~(1 << 11); /* Enable bus access */
+    if (__has_colors) {
+        value = (unsigned *)0x900C0004;
+        control = (unsigned *)0x900C0008;
+        *(volatile unsigned *)0x900C0080 = 0xA; /* "[...] on the CX to specify the 32768Hz clock as the source for the First Timer" */
+        *control = 0b10000010;                  /* Wrapping; 32-bit; divider to 1; interrupt disabled; free-running; start */
+        start = *value;
+    } else {
+        value = (unsigned *)0x900C0000;
+        control = (unsigned *)0x900C0008;
+        *(volatile unsigned *)0x900C0004 = 33; /* Set time divider to 33; 32768/33=993Hz (approx. 1000Hz) */
+        *control = 0b00001111;                 /* Run infinitely; increasing; start */
+        *value = 0;
+    }
 }
 
-Uint32 SDL_GetTicks (void)
-{
-	if ( __has_colors )
-		return((start - *value) / 33);
-	else {
-		tick_sum += *value;
-		*value = 0;
-		return(tick_sum);
-	}
+Uint32 SDL_GetTicks(void) {
+    if (__has_colors)
+        return ((start - *value) / 33);
+    else {
+        tick_sum += *value;
+        *value = 0;
+        return (tick_sum);
+    }
 }
 
-void SDL_Delay (Uint32 ms)
-{
-	sleep(ms);
+void SDL_Delay(Uint32 ms) {
+    msleep(ms);
 }
 
 #include "SDL_thread.h"
@@ -79,45 +77,40 @@ void SDL_Delay (Uint32 ms)
 static int timer_alive = 0;
 static SDL_Thread *timer = NULL;
 
-static int RunTimer(void *unused)
-{
-	while ( timer_alive ) {
-		if ( SDL_timer_running ) {
-			SDL_ThreadedTimerCheck();
-		}
-		SDL_Delay(1);
-	}
-	return(0);
+static int RunTimer(void *unused) {
+    while (timer_alive) {
+        if (SDL_timer_running) {
+            SDL_ThreadedTimerCheck();
+        }
+        SDL_Delay(1);
+    }
+    return (0);
 }
 
 /* This is only called if the event thread is not running */
-int SDL_SYS_TimerInit(void)
-{
-	timer_alive = 1;
-	timer = SDL_CreateThread(RunTimer, NULL);
-	if ( timer == NULL )
-		return(-1);
-	return(SDL_SetTimerThreaded(1));
+int SDL_SYS_TimerInit(void) {
+    timer_alive = 1;
+    timer = SDL_CreateThread(RunTimer, NULL);
+    if (timer == NULL)
+        return (-1);
+    return (SDL_SetTimerThreaded(1));
 }
 
-void SDL_SYS_TimerQuit(void)
-{
-	timer_alive = 0;
-	if ( timer ) {
-		SDL_WaitThread(timer, NULL);
-		timer = NULL;
-	}
+void SDL_SYS_TimerQuit(void) {
+    timer_alive = 0;
+    if (timer) {
+        SDL_WaitThread(timer, NULL);
+        timer = NULL;
+    }
 }
 
-int SDL_SYS_StartTimer(void)
-{
-	SDL_SetError("Timers not implemented on the TI-Nspire");
-	return(-1);
+int SDL_SYS_StartTimer(void) {
+    SDL_SetError("Timers not implemented on the TI-Nspire");
+    return (-1);
 }
 
-void SDL_SYS_StopTimer(void)
-{
-	return;
+void SDL_SYS_StopTimer(void) {
+    return;
 }
 
 #endif /* SDL_TIMER_TINSPIRE */
